6872778950ad3931c10375f602f53f293c687cd7
Vibhav <vibhavsachan743@gmail.com>
2023-03-22 23:18:49 +0530

    Home fragment, Retrofit, random image

--- app/src/main/java/com/vibhav/instafoodie/activity/MainActivity.kt
+++ app/src/main/java/com/vibhav/instafoodie/activity/MainActivity.kt
@@ -1,18 +1,19 @@
-package com.vibhav.instafoodie
+package com.vibhav.instafoodie.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.vibhav.instafoodie.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottomNav)
-        val navController = Navigation.findNavController(this,R.id.host_fragment)
+        val bottomNavigation = findViewById<BottomNavigationView  >(R.id.bottomNav)
+        val navController = Navigation.findNavController(this, R.id.host_fragment)
 
         NavigationUI.setupWithNavController(bottomNavigation, navController)
     }
